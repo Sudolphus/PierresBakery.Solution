6f9d6d2966aa0f74d609389d7e48d5f1a213f975
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-17 10:00:06 -0700

    add test for calculating price of bread

--- PierresBakery.Tests/ModelTests/OrderTests.cs
+++ PierresBakery.Tests/ModelTests/OrderTests.cs
@@ -20,5 +20,20 @@ namespace PierresBakery.Tests
       Assert.AreEqual(3, newUserOrder.Bread);
       Assert.AreEqual(5, newUserOrder.Pastry);
     }
+
+    [DataRow(0, 0)]
+    [DataRow(1, 5)]
+    [DataRow(2, 10)]
+    [DataRow(3, 10)]
+    [DataRow(4, 15)]
+    [DataRow(20, 70)]
+    [DataRow(90, 300)]
+    [DataTestMethod]
+    public void PriceOfBread_ShouldCalculateBreadPrice_Match(int bread, int expectedBreadPrice)
+    {
+      Order newOrder = new Order(bread, 5);
+      int calculatedBreadPrice = newOrder.PriceOfBread();
+      Assert.AreEqual(expectedBreadPrice, calculatedBreadPrice);
+    }
   }
 }
