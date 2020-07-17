63228b54e45daf6bd8a13677377fcf2acb15fa53
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-17 09:44:18 -0700

    add constructor for Order object

--- PierresBakery/Models/Order.cs
+++ PierresBakery/Models/Order.cs
@@ -2,6 +2,13 @@ namespace PierresBakery.Models
 {
   public class Order
   {
-    
+    public int Bread { get; set; }
+    public int Pastry { get; set; }
+
+    public Order(int bread, int pastry)
+    {
+      Bread = bread;
+      Pastry = pastry;
+    }
   }
 }
